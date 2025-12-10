9df52eed3e31f992b10935dcf98480b27695df51
Marc <marc.paelinck@gmail.com>
2025-12-10 15:57:49 +0100

    modified package config for more recent ECMAscript package

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
+import js from '@eslint/js'
+import globals, { es2024, es2025 } from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
 export default tseslint.defineConfig([
     globalIgnores(['dist']),
@@ -15,9 +15,6 @@ export default tseslint.defineConfig([
             reactHooks.configs['recommended-latest'],
             reactRefresh.configs.vite
         ],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser
-        }
+        languageOptions: { ecmaVersion: 5, globals: globals.browser }
     }
-]);
+])
