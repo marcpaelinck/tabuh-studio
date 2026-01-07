3d8827c0cfa6c10f660991c175ec30bfc2694806
Marc <marc.paelinck@gmail.com>
2026-01-07 11:14:11 +0100

    adapted debugger for V8  browsers (Chrome)

--- src/utils/debugger.ts
+++ src/utils/debugger.ts
@@ -7,8 +7,8 @@ const debugOn: Record<string, boolean> = {
     Menu: false,
     MeasureNode: false,
     NavigationCell: false,
-    PlaybackButtons: false,
-    playbackReducer: false,
+    PlaybackButtons: true,
+    playbackReducer: true,
     registerComponent: false,
     score: false,
     ScorePlayer: false,
@@ -22,22 +22,39 @@ const debugOn: Record<string, boolean> = {
 }
 
 // regex used to parse call stack to determine caller module
-const regExp: RegExp = /([\w_]+).*@[\w/:\.\-_]+\/([\w_\-]+)\.\w+\?/g
+// const regExp: RegExp = /([\w_]+).*@[\w/:\.\-_]+\/([\w_\-]+)\.\w+\?/g
+const regExpLineSplit: RegExp = /(.+)(?:[\n\r]+|$)/g
+//JavaScriptCore (a.o. Firefox)
+const regExpJSC: RegExp = /([\w_\-\/<]+)@.+?([\w_\-]+)\.(?:ts|tsx|js)/g
+//V0 (a.o. Chrome)
+const regExpV8: RegExp = / +at +([\w_\-]+).+?([\w_\-]+)\.(?:ts|tsx|js)/g
 
 export const debug = (message: any, raw: boolean = false) => {
     const stack = new Error().stack as string
-    const matches = [...stack?.matchAll(regExp)]
-    const caller = matches.length >= 2 ? matches[1][2] : ''
+    // Split in separate txt lines and select the second line:
+    // the first line contains the trace for the current function call (debug)
+    const linematches = [...stack?.matchAll(regExpLineSplit)].map((match) => match[1])
+    // Parse the trace message. Try two formats
+    var trace = [...linematches[1]?.matchAll(regExpJSC)]
+    if (trace.length == 0) trace = [...linematches[2]?.matchAll(regExpV8)]
+    // Next to the original string, parsing should return two captured values:
+    // the calling function and the name of its module
+    if (!trace || trace.length == 0 || trace[0].length < 3) {
+        console.log('debug: could not determine caller.')
+        console.log(linematches[1])
+        console.log(trace)
+        return
+    }
+    const [func, module]: string[] = trace[0].slice(1, 3)
+    // Check if the module is in the above debugOn list
+    if (!(module in debugOn)) console.log(`debug: no entry for ${module}.`)
 
-    if (!caller) console.log('debug: could not determine caller.')
-    if (!(caller in debugOn)) console.log(`debug: no entry for ${caller}.`)
-
-    if (debugOn[caller]) {
+    if (debugOn[module]) {
         if (raw) {
             console.log(message)
             return
         }
-        const callerTxt = caller ? `${matches[1][1]} [${matches[1][2]}]: ` : ''
+        const callerTxt = module ? `${func} [${module}]` : ''
         var logText = message
         if (_.isArray(message) || _.isObject(message)) logText = JSON.stringify(message)
         console.log(`${callerTxt}: ${logText}`)
