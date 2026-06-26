ec3c1da80a04c46208208f9f4bfd141ba4b898ee
Marc <marc.paelinck@gmail.com>
2026-06-26 10:53:29 +0200

    addec comment

--- frontend/src/components/Feature.tsx
+++ frontend/src/components/Feature.tsx
@@ -1,6 +1,10 @@
 import type { PropsWithChildren } from 'react'
 import { useEnvironmentManager } from '../stores/useEnvironmentManager'
 
+// Wrap a component with this element if it should not be deployed in production environment.
+// <FeatureUnderDevelopment>
+//   <NewFeatureComponent>
+// <FeatureUnderDevelopment/>
 export const FeatureUnderDevelopment: React.FC<PropsWithChildren> = (props: PropsWithChildren) => {
     const { environment } = useEnvironmentManager()
     if (environment == 'production') return null
